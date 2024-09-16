fb3ff88e62a25654af897d2cfdc69076efb950ca
samlan24 <muchemionline@gmail.com>
2024-09-16 18:49:53 +0300

    setting averages

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+  const handleGood = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+  }
+  const handleNeutral = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+  }
+  const handleBad = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+  }
+
+  const total = good + neutral + bad;
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={handleGood} >good</button>
+      <button onClick={handleNeutral} >neutral</button>
+      <button onClick={handleBad} >bad</button>
+      <h2>statistics</h2>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+      <p>all: {total}</p>
+    </div>
+  )
+}
+
+export default App
