d5a1b5608bfade21322355a1cc05ce938898f4bf
samlan24 <muchemionline@gmail.com>
2024-10-14 11:15:26 +0300

    added the filter method

--- part2/phonebook-app/src/App.jsx
+++ part2/phonebook-app/src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-1234567' }]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [filterName, setFilterName] = useState('');
 
   // checks if name exists
   const nameExists = new Set(persons.map(person => person.name));
@@ -34,9 +35,25 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleFilterName = (event) => {
+    setFilterName(event.target.value);
+  };
+
+  // Filtering persons based on the search input
+  const personsToShow = persons.filter(person =>
+    person.name.toLowerCase().includes(filterName.toLowerCase())
+  );
+
   return (
     <div>
       <h2>Phonebook</h2>
+
+
+      <div>
+        Who to see: <input value={filterName} onChange={handleFilterName} />
+      </div>
+
+      <h2>Add New User</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handlePersonChange} />
@@ -48,10 +65,14 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
+
       <ul>
-        {persons.map((person, index) => (
-          <li key={index}>{person.name} {person.number}</li>
+        {personsToShow.map((person, index) => (
+          <li key={index}>
+            {person.name} {person.number}
+          </li>
         ))}
       </ul>
     </div>
